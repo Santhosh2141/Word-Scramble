f2403855700625e12c7568c2c5a4d20110d43849
Santhosh2141 <snthshsrinivas@gmail.com>
2025-06-11 19:53:06 +0530

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Santhosh Srinivas on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
