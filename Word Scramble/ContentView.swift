db4ed50642306b493495a7da5aaf801f9e2c17a2
Santhosh2141 <snthshsrinivas@gmail.com>
2025-06-11 23:48:45 +0530

    display the root word and checked if word entered is real, duplicate, possible..

--- Word Scramble/ContentView.swift
+++ Word Scramble/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMsg = ""
+    @State private var showError = false
     
     var body: some View {
         NavigationStack{
@@ -52,8 +55,13 @@ struct ContentView: View {
                 
             }
             .navigationTitle(rootWord)
-            .onSubmit {
-                addNewWord()
+            .onSubmit(addNewWord)
+            .onAppear(perform: pickWord)
+            .alert(errorTitle,isPresented: $showError){
+//                Button("Ok") { }
+                // if no button, alert gives an automatic ok button
+            } message: {
+                Text(errorMsg)
             }
         }
         
@@ -63,11 +71,69 @@ struct ContentView: View {
         
         guard rawWord.count > 0 else { return }
         
+        guard isOriginal(word: rawWord) else{
+            wordError(title: "Duplicate Word", msg: "Word is already present in the list")
+            return
+        }
+        
+        guard isReal(word: rawWord) else {
+            wordError(title: "Fake Word", msg: "Word entered is not a real word")
+            return
+        }
+        
+        guard possibleWord(word: rawWord) else {
+            wordError(title: "Impossible Word", msg: "Word is not possible from \(rootWord)")
+            return
+        }
+        
         withAnimation{
             usedWords.insert(rawWord, at: 0)
         }
         newWord = ""
         
+    }
+    func pickWord() {
+        // if there is some major issue w the file, we can throw a fatalError which crashes the app totally
+        if let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let fileContents = try? String(contentsOf: fileURL){
+                let allWords = fileContents.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkWorm"
+                
+                return
+            }
+        }
+        fatalError("Cant Load start.txt from bundle")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func possibleWord(word: String) -> Bool{
+        var rootWordCopy = rootWord
+        
+        for letter in word{
+            if let pos = rootWordCopy.firstIndex(of: letter){
+                rootWordCopy.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledWord = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledWord.location == NSNotFound
+        
+    }
+    
+    func wordError(title: String, msg: String){
+        errorTitle = title
+        errorMsg = msg
+        showError = true
     }
 //    func testBundles() {
 //
