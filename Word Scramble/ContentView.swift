5ef4928a53b21bd973df50c8a12b406124a69c89
Santhosh2141 <snthshsrinivas@gmail.com>
2025-06-11 20:35:27 +0530

    Created Lists, Bundles(work w files), working w strings

--- Word Scramble/ContentView.swift
+++ Word Scramble/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let b99 = ["Jake", "Amy", "Charles", "Terry"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//            List() {
+//                Section("Section 2"){
+//                    Text("Static Row 1")
+//                    Text("Static Row 2")
+//                    Text("Static Row 3")
+//                    Text("Static Row 4")
+//                }
+//                Section("Section 1"){
+//                    ForEach(0..<5){
+//                        Text("Dynamic Row \($0+1)")
+//                    }
+//                }
+//            }
+//            .listStyle(.grouped)
+            List(b99, id: \.self){
+                Text($0)
+            }
+            
         }
-        .padding()
+    }
+    func testBundles() {
+        
+        // This bundle can have a file or cannot have a file. so optional. resoure is the name of the file and extension is the extension
+        if let fileURL = Bundle.main.url(forResource: "someFile", withExtension: "txt"){
+//            fileURL gets the link to the file. and string contentsOf converts it to a string
+            if let fileContents = try? String(contentsOf: fileURL){
+                // If it has the cotnents it can execute it. if it doesnt it throws an error
+            }
+        }
+    }
+    func testString() {
+        let input = "a b c"
+        let mlInput = """
+        a
+        b
+        c
+        """
+        let array = input.components(separatedBy: " ")
+        let array2 = mlInput.components(separatedBy: "\n")
+        let letter = array.randomElement()
+        // randomElement returns an optional string as the array might be empty so we need to unwrap it using nil coelescing so we use ?.
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let word = "swift"
+        let checker = UITextChecker()
+        // SWIFT allows using complex chars like emojis in a string. so we need to convert the swift string to an Objcetive C string to check it using below:
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        // this returns if a misspelled word is found as a range
+        let allGood = misspelledRange.location == NSNotFound
+        // this NSNotFound is sent if there is no spelling mistake
     }
 }
 
